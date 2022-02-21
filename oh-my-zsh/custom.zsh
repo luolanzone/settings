c5c05075228ba3d944872c91eca00bde2ae39637
luolanzone <luolanzone@gmail.com>
2022-02-21 21:39:50 +0800

    add more functions

--- oh-my-zsh/custom.zsh
+++ oh-my-zsh/custom.zsh
@@ -9,26 +9,13 @@ alias kc="kubectl create"
 alias gs="git status"
 alias glh="git log --graph --oneline --decorate | head"
 # custom functions
-kg() {
+# kubectl
+function kg() {
   command kubectl get -o wide "$@"
 }
-kgp() {
+
+function kgp() {
   command kubectl get pods -o wide "$@"
-}
-touch () {
-    command touch "$@" && code "$@"
-}
-
-function hex2ip() {
-  printf '%d.%d.%d.%d\n' $(echo $1 | sed 's/../0x& /g')
-}
-
-function ip2hex() {
-  printf '%02x%02x%02x%02x' $(echo $1  | awk -F. '{print $1" "$2" "$3" "$4}')
-}
-
-function inspect() {
-  docker inspect --format "{{ .State.Pid }}" $1
 }
 
 function kgs() {
@@ -46,6 +33,17 @@ function kgetall {
   done
 }
 
+function rmp () {
+  kubectl get pods -l $1 -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | xargs -n 1 kubectl delete pod
+}
+
+function getimgs(){
+  # kubectl set image deployment/nginx-deployment nginx=nginx:1.16.1
+  # https://kubernetes.io/docs/tasks/access-application-cluster/list-all-running-container-images/
+  kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}" -l $1 | tr -s '[[:space:]]' '\n' | sort | uniq -c
+}
+
+# docker
 function nsenter-ctn () {
     CTN=$1  # container ID or name
     PID=$(sudo docker inspect --format "{{.State.Pid}}" $CTN)
@@ -53,6 +51,43 @@ function nsenter-ctn () {
     nsenter -t $PID $@
 }
 
-function rmp () {
-  kubectl get pods -l $1 -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | xargs -n 1 kubectl delete pod
+function getpid() {
+  docker inspect --format "{{ .State.Pid }}" $1
+}
+
+function getip(){
+  docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $1
+}
+
+function getips(){
+  docker inspect -f '{{$.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -q)
+}
+
+function cleanup(){
+  docker ps --filter status=exited -q | xargs docker rm
+}
+
+# network utils
+function hex2ip() {
+  printf '%d.%d.%d.%d\n' $(echo $1 | sed 's/../0x& /g')
+}
+
+function ip2hex() {
+  printf '%02x%02x%02x%02x' $(echo $1  | awk -F. '{print $1" "$2" "$3" "$4}')
+}
+
+# misc
+function touch () {
+    command touch "$@" && code "$@"
+}
+
+# list and sort directory based on disk size
+function du1(){
+  os=$(uname)
+  if [[ $os == "Darwin" ]];then
+    du -h -d 1 | sort -hr
+  fi
+  if [[ $os == "Linux" ]];then
+     du -h --max-depth=1 | sort -hr
+  fi
 }
